e8da1013ee65b8af37ff90d732f410345c18fb8e
POWER Alexander <71255126+apowapow@users.noreply.github.com>
2020-09-14 16:09:40 +0100

    part 3 finished

--- main.py
+++ main.py
@@ -1,5 +1,30 @@
+import re
+
+FILE_SAMPLE = "sample.txt"
+READ_ONLY = "r"
+
+
 def main():
-    pass
+    d = {}
+
+    with open(FILE_SAMPLE, READ_ONLY) as f:
+        file_content = f.read().replace('\n', ' ').split(' ')
+
+    for fc in file_content:
+        result = re.findall("^[a-zA-Z0-9.'_%+-]+[@]{1}[a-zA-Z0-9.'_%+-]+$", fc)
+
+        if len(result) > 0:
+            fc_partition = fc.partition("@")
+            local = fc_partition[0]
+            domain = fc_partition[2]
+
+            if domain not in d.keys():
+                d[domain] = []
+
+            d[domain].append(local)
+
+    for domain, local in d.items():
+        print("{0}: {1}".format(domain, len(local)))
 
 
 if __name__ == "__main__":
